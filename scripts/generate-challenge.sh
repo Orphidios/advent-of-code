f81bd71618f7db0311b7fadf5591bbcdc690a6d1
orphidios <sylvain.r@padoa-group.com>
2024-12-18 12:06:21 +0100

    feat: add script to generate challenge files

---
+++ scripts/generate-challenge.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Check if a day number is provided
+if [ -z "$1" ]; then
+  echo "Please provide a day number."
+  exit 1
+fi
+
+DAY_NUMBER=$1
+FILE_NAME="day${DAY_NUMBER}.challenge.ts"
+FILE_PATH="./src/challenges/${FILE_NAME}"
+
+# Create the file with the template content
+cat <<EOL > $FILE_PATH
+import { Challenge } from "../common/challenge.class.js";
+
+type Day${DAY_NUMBER}Input = {
+    // Define the input structure here
+}
+
+export class Day${DAY_NUMBER}Challenge extends Challenge<Day${DAY_NUMBER}Input> {
+    DAY = ${DAY_NUMBER};
+
+    protected runWithInput(input: Day${DAY_NUMBER}Input): void {
+        // Implement the logic here
+    }
+
+    protected parseInput(input: string): Day${DAY_NUMBER}Input {
+        console.log('Raw input file for day ${DAY_NUMBER}', input);
+        // Parse the input string and return the input object
+        return {} as Day${DAY_NUMBER}Input;
+    }
+}
+
+new Day${DAY_NUMBER}Challenge().run();
+EOL
+
+echo "Challenge file for Day ${DAY_NUMBER} created successfully at ${FILE_PATH}"
--- src/challenges/day2.challenge.ts
+++ src/challenges/day2.challenge.ts
@@ -1,7 +1,20 @@
-class Day2Challenge {
-  static run() {
-    console.log('Day 2 challenge');
-  }
+import { Challenge } from "../common/challenge.class.js";
+
+type Day2Input = {
+    // Define the input structure here
 }
 
-Day2Challenge.run();
+class Day2Challenge extends Challenge<Day2Input> {
+    DAY = 2;
+
+    protected runWithInput(input: Day2Input): void {
+        // Implement the logic here
+    }
+
+    protected parseInput(input: string): Day2Input {
+        // Parse the input string and return the input object
+        return {} as Day2Input;
+    }
+}
+
+export { Day2Challenge };
