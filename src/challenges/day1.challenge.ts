cf291b8a99447d7381a596825fd43701ecc398fb
orphidios <sylvain.r@padoa-group.com>
2024-12-19 10:51:28 +0100

    refactor(day1): improve input parsing and distance calculation methods

--- src/challenges/day1.challenge.ts
+++ src/challenges/day1.challenge.ts
@@ -14,33 +14,32 @@ class Day1Challenge extends Challenge<Day1Input> {
     }
 
     protected parseInput(input: string): Day1Input {
-        return input.split('\n').filter((line) => line != '').reduce((acc: Day1Input, line) => {
-            const [left, right] = line.split('  ').map(Number);
-            acc.leftList.push(left);
-            acc.rightList.push(right);
-            return acc;
-        }, { leftList: [], rightList: [] });
+        return input.split('\n')
+            .filter(line => line !== '')
+            .reduce((acc: Day1Input, line) => {
+                const [left, right] = line.split('  ').map(Number);
+                acc.leftList.push(left);
+                acc.rightList.push(right);
+                return acc;
+            }, { leftList: [], rightList: [] });
     }
 
-    protected distanceBetweenList(ListA: number[], ListB: number[]): number {
-        const sortedLeftList = ListA.sort();
-        const sortedRightList = ListB.sort();        
-        return sortedLeftList.reduce((acc, current, index) => acc + Math.abs(sortedLeftList[index] - sortedRightList[index]), 0);
+    protected distanceBetweenList(listA: number[], listB: number[]): number {
+        const sortedLeftList = [...listA].sort((a, b) => a - b);
+        const sortedRightList = [...listB].sort((a, b) => a - b);
+        return sortedLeftList.reduce((acc, current, index) => acc + Math.abs(current - sortedRightList[index]), 0);
     }
-    
+
     protected similarityBetweenList(leftList: number[], rightList: number[]): number {
         const referenceList = this.countUnique(rightList);
         return leftList.reduce((acc, value) => acc + (referenceList[value] ? value * referenceList[value] : 0), 0);
     }
 
     protected countUnique(arrayToCount: number[]): { [key: number]: number } {
-        return arrayToCount.reduce((acc: { [key: number]: number }, element: number): { [key: number]: number } => {
-            const previousValue = acc[element] ? acc[element] : 0;
-            return {
-                ...acc,
-                [element]: previousValue + 1,
-            }
-            }, {});
+        return arrayToCount.reduce((acc: { [key: number]: number }, element: number) => {
+            acc[element] = (acc[element] || 0) + 1;
+            return acc;
+        }, {});
     }
 }
 
